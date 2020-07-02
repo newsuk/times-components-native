f8b182f3eaa8c085387e657e023cdfad97c021d0
Emmanouil Konstantinidis <hello@manos.im>
2020-07-02 09:35:44 +0100

    chore: Remove all web test suites

--- packages/article-list/__tests__/web/jest.config.js
+++
@@ -1,9 +0,0 @@
-const jestConfigurator = require("@times-components-native/jest-configurator")
-  .default;
-
-module.exports = jestConfigurator("web", __dirname, {
-  coverageIgnoreGlobs: [
-    "article-list-item-prop-types.js",
-    "article-list-item-prop-types.web.js"
-  ]
-});
--- packages/image/__tests__/web/jest.config.js
+++
@@ -1,6 +0,0 @@
-const jestConfigurator = require("@times-components-native/jest-configurator")
-  .default;
-
-module.exports = jestConfigurator("web", __dirname, {
-  coverageIgnoreGlobs: ["index.js", "modal-image.web.js"]
-});
