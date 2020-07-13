da699932591cbc674e38f3c49449a92518d97cec
Emmanouil Konstantinidis <hello@manos.im>
2020-07-13 17:26:57 +0100

    chore: Fix eslint config & apply prettier

--- doc/running-ios-dev.md
+++ doc/running-ios-dev.md
@@ -1,11 +1,13 @@
 # How to get TC running on IOS
 
 ## Prequisites
-* clone https://github.com/newsuk/times-components
-* clone https://github.com/newsuk/nuk-tnl-app-ios-universal/
-* brew install cocoapods (https://brew.sh)
+
+- clone https://github.com/newsuk/times-components
+- clone https://github.com/newsuk/nuk-tnl-app-ios-universal/
+- brew install cocoapods (https://brew.sh)
 
 The directory structure should look like:
+
 ```
 root
   | -> times-components
@@ -13,29 +15,36 @@ root
 ```
 
 ## Set up Times-Components
+
 ```
 cd times-components
 GRAPHQL_ENDPOINT="https://api.thetimes.co.uk/graphql" yarn
 ```
 
 ## Set up the IOS App
+
 ```
 cd nuk-tnl-app-ios-universal
 pod update
 ```
 
 ## Running them together with hot-reload
+
 ### In TimesComponents
+
 ```
 yarn run ios:app
 ```
+
 ### In IOS App
+
 ```
 open folder with xcode
 press the play button in xcode to run the app
 ```
 
 Once in the app, go to hardware->shake gesture and then press (configure bundler), use the following settings:
+
 ```
 0.0.0.0
 8081
--- test-setup/jest.android.config.js
+++ test-setup/jest.android.config.js
@@ -1,23 +1,28 @@
 module.exports = {
   haste: {
-    defaultPlatform: 'android',
-    platforms: ['android'],
-    providesModuleNodeModules: ['react-native']
+    defaultPlatform: "android",
+    platforms: ["android"],
+    providesModuleNodeModules: ["react-native"]
   },
-  preset: 'react-native',
+  preset: "react-native",
   setupFiles: [
-    '<rootDir>/test-setup/setup-jest.js',
-    'jest-plugin-context/setup'
+    "<rootDir>/test-setup/setup-jest.js",
+    "jest-plugin-context/setup"
   ],
   setupFilesAfterEnv: [],
-  rootDir: '../',
-  testMatch: ['**/__tests__/android/*.test.js', '**/__tests__/android/*.test.ts'],
-  testURL: 'http://localhost',
+  rootDir: "../",
+  testMatch: [
+    "**/__tests__/android/*.test.js",
+    "**/__tests__/android/*.test.ts"
+  ],
+  testURL: "http://localhost",
   transform: {
-    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$': '<rootDir>/node_modules/react-native/jest/assetFileTransformer.js',
-    '\\.(gql|graphql)$': 'jest-transform-graphql',
-    '^.+\\.graphql': 'babel-jest'
+    "^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$":
+      "<rootDir>/node_modules/react-native/jest/assetFileTransformer.js",
+    "\\.(gql|graphql)$": "jest-transform-graphql",
+    "^.+\\.graphql": "babel-jest"
   },
-  transformIgnorePatterns: ['node_modules/(?!(react-native|react-native-svg|react-native-webview|react-native-autoheight-webview|react-native-iphone-x-helper|@storybook/react-native|react-native-swipe-gestures|react-native-device-info)/)']
-}
-
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native|react-native-svg|react-native-webview|react-native-autoheight-webview|react-native-iphone-x-helper|@storybook/react-native|react-native-swipe-gestures|react-native-device-info)/)"
+  ]
+};
