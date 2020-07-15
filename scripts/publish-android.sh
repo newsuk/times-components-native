ded6ba611bc12309022a4f1b52213a1a896491ba
The Times Tools <tools@news.co.uk>
2020-07-15 15:31:29 +0100

    chore: Move publish scripts (iOS & Android)

--- scripts/publish-android.sh
+++ scripts/publish-android.sh
@@ -8,10 +8,10 @@ function publish {
   curl -T $FILEPATH --header "X-Explode-Archive: true" -u${ARTIFACTORY_USER}:${ARTIFACTORY_API_KEY} "$ARTIFACTORY_URL"
 }
 
-echo "Publishing to bintray $ARTIFACTORY_URL"
-ROOT_DIR=$(dirname "$0")
+echo "Publishing to jfrog $ARTIFACTORY_URL"
+LIB_DIR="./lib/android"
 
 # Upload contents of repo folder
-cd $ROOT_DIR/repo
+cd "$LIB_DIR/repo"
 ls -d */ | cut -f1 -d'/' | while read line ; do publish $line ; done
-cd $ROOT_DIR
+cd -
