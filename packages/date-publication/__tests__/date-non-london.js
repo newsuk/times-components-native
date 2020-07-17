83acb71b3bd4e595810c99e250369aa383757e43
Geoff-Ford <me@geoffford.co.uk>
2020-07-17 09:40:02 +0100

    chore: Enabled eslint no-redeclare and fixed related issues

--- packages/date-publication/__tests__/date-non-london.js
+++ packages/date-publication/__tests__/date-non-london.js
@@ -1,4 +1,4 @@
-/* global Intl */
+// global Intl
 import { iterator } from "@times-components-native/test-utils";
 import { isLondonTimezone } from "../src/date";
 import "./mock-timezone-non-london";
@@ -10,8 +10,8 @@ export default () => {
       name: "should return false when London is NOT the timezone",
       test() {
         expect(isLondonTimezone()).toEqual(false);
-      }
-    }
+      },
+    },
   ];
 
   iterator(tests);
