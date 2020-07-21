2ef7da80ca658b3dd79eb53b6038dee13c9d9f57
Chris Jordan <chris.jordan@news.co.uk>
2020-07-21 16:21:51 +0100

    fix: fixes article-skeleton storybook (#52)

--- storybook-native/index.js
+++ storybook-native/index.js
@@ -2,5 +2,11 @@ import "core-js";
 import "regenerator-runtime/runtime";
 import { AppRegistry } from "react-native";
 import StorybookUIRoot from "./storybook";
+import {FontStorage} from "@times-components-native/typeset";
+import ttf from "../fonts";
+
+Object.keys(ttf).forEach((fontName) => {
+  FontStorage.registerFont(fontName, ttf[fontName]);
+});
 
 AppRegistry.registerComponent("storybooknative", () => StorybookUIRoot);
