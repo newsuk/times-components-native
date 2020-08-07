7ce32088acc19b11dfc5c56e2785e7cc063c7486
Aron Shamash <aronshamash@gmail.com>
2020-08-07 14:52:04 +0100

    feat (TNLT-3577): change order and tile for lead2nopicand2 in portrait tablet

---
+++ storybook-native/story-loader.js
@@ -0,0 +1,126 @@
+
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../packages/ad/ad.stories');
+  require('../packages/article-byline/article-byline-opinion.stories');
+  require('../packages/article-byline/article-byline-with-links.stories');
+  require('../packages/article-byline/article-byline.stories');
+  require('../packages/article-comments/article-comments.stories');
+  require('../packages/article-error/article-error.stories');
+  require('../packages/article-extras/article-extras.stories');
+  require('../packages/article-flag/article-flag.stories');
+  require('../packages/article-image/article-image.stories');
+  require('../packages/article-in-depth/article-in-depth.stories');
+  require('../packages/article-label/article-label.stories');
+  require('../packages/article-lead-asset/article-lead-asset.stories');
+  require('../packages/article-list/article-list.stories');
+  require('../packages/article-magazine-comment/article-magazine-comment.stories');
+  require('../packages/article-magazine-standard/article-magazine-standard.stories');
+  require('../packages/article-comment-tablet/article-main-comment.stories');
+  require('../packages/article-main-comment/article-main-comment.stories');
+  require('../packages/article-main-standard/article-main-standard.stories');
+  require('../packages/article-paragraph/article-paragraph.stories');
+  require('../packages/article-skeleton/article-skeleton.stories');
+  require('../packages/article-summary/article-summary.stories');
+  require('../packages/article-topics/article-topics.stories');
+  require('../packages/article/article.stories');
+  require('../packages/author-profile/author-profile.stories');
+  require('../packages/button/button.stories');
+  require('../packages/caption/caption.stories');
+  require('../packages/card/card.stories');
+  require('../packages/date-publication/date-publication.stories');
+  require('../packages/edition-slices/edition-puzzle-tiles.stories');
+  require('../packages/edition-slices/edition-puzzles.stories');
+  require('../packages/edition-slices/edition-slices.stories');
+  require('../packages/edition-slices/edition-tiles.stories');
+  require('../packages/error-view/error-view.stories');
+  require('../packages/gradient/gradient.stories');
+  require('../packages/icons/icons.stories');
+  require('../packages/image/image.stories');
+  require('../packages/image/modal-image.stories');
+  require('../packages/interactive-wrapper/interactive-wrapper.stories');
+  require('../packages/key-facts/key-facts.stories');
+  require('../packages/link/link.stories');
+  require('../packages/markup/markup.stories');
+  require('../packages/message-bar/message-bar.stories');
+  require('../packages/provider/provider.stories');
+  require('../packages/pull-quote/pull-quote.stories');
+  require('../packages/related-articles/related-articles.stories');
+  require('../packages/section/section.stories');
+  require('../packages/slice-layout/slice-layout.stories');
+  require('../packages/star-button/star-button.stories');
+  require('../packages/styleguide/styleguide.stories');
+  require('../packages/topic/topic.stories');
+  require('../packages/tracking/tracking.stories');
+  require('../packages/video-label/video-label.stories');
+  require('../packages/video/video.stories');
+  require('../packages/watermark/watermark.stories');
+  
+}
+
+const stories = [
+  '../packages/ad/ad.stories',
+  '../packages/article-byline/article-byline-opinion.stories',
+  '../packages/article-byline/article-byline-with-links.stories',
+  '../packages/article-byline/article-byline.stories',
+  '../packages/article-comments/article-comments.stories',
+  '../packages/article-error/article-error.stories',
+  '../packages/article-extras/article-extras.stories',
+  '../packages/article-flag/article-flag.stories',
+  '../packages/article-image/article-image.stories',
+  '../packages/article-in-depth/article-in-depth.stories',
+  '../packages/article-label/article-label.stories',
+  '../packages/article-lead-asset/article-lead-asset.stories',
+  '../packages/article-list/article-list.stories',
+  '../packages/article-magazine-comment/article-magazine-comment.stories',
+  '../packages/article-magazine-standard/article-magazine-standard.stories',
+  '../packages/article-main-comment-tablet/article-main-comment.stories',
+  '../packages/article-main-comment/article-main-comment.stories',
+  '../packages/article-main-standard/article-main-standard.stories',
+  '../packages/article-paragraph/article-paragraph.stories',
+  '../packages/article-skeleton/article-skeleton.stories',
+  '../packages/article-summary/article-summary.stories',
+  '../packages/article-topics/article-topics.stories',
+  '../packages/article/article.stories',
+  '../packages/author-profile/author-profile.stories',
+  '../packages/button/button.stories',
+  '../packages/caption/caption.stories',
+  '../packages/card/card.stories',
+  '../packages/date-publication/date-publication.stories',
+  '../packages/edition-slices/edition-puzzle-tiles.stories',
+  '../packages/edition-slices/edition-puzzles.stories',
+  '../packages/edition-slices/edition-slices.stories',
+  '../packages/edition-slices/edition-tiles.stories',
+  '../packages/error-view/error-view.stories',
+  '../packages/gradient/gradient.stories',
+  '../packages/icons/icons.stories',
+  '../packages/image/image.stories',
+  '../packages/image/modal-image.stories',
+  '../packages/interactive-wrapper/interactive-wrapper.stories',
+  '../packages/key-facts/key-facts.stories',
+  '../packages/link/link.stories',
+  '../packages/markup/markup.stories',
+  '../packages/message-bar/message-bar.stories',
+  '../packages/provider/provider.stories',
+  '../packages/pull-quote/pull-quote.stories',
+  '../packages/related-articles/related-articles.stories',
+  '../packages/section/section.stories',
+  '../packages/slice-layout/slice-layout.stories',
+  '../packages/star-button/star-button.stories',
+  '../packages/styleguide/styleguide.stories',
+  '../packages/topic/topic.stories',
+  '../packages/tracking/tracking.stories',
+  '../packages/video-label/video-label.stories',
+  '../packages/video/video.stories',
+  '../packages/watermark/watermark.stories',
+  
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
