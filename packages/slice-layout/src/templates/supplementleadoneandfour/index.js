48c91d6d45ebb18123c0c4cb6cd9ccb884d83782
Aron Shamash <aronshamash@gmail.com>
2020-08-28 10:49:38 +0100

    feat: logic for supplement checks

--- packages/slice-layout/src/templates/supplementleadoneandfour/index.js
+++ packages/slice-layout/src/templates/supplementleadoneandfour/index.js
@@ -1,12 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { View } from "react-native";
-import { editionBreakpoints } from "@times-components-native/styleguide";
 import { ItemColSeparator } from "../shared";
+import HorizontalLayout from "../horizontallayout";
 import VerticalLayout from "../verticallayout";
 import stylesFactory from "./styles";
 
 const SupplementLeadOneAndFourSlice = ({
   breakpoint,
+  orientation,
   lead,
   support1,
   support2,
@@ -14,24 +15,32 @@ const SupplementLeadOneAndFourSlice = ({
   support4,
 }) => {
   const styles = stylesFactory(breakpoint);
-  const { container, leadContainer, supportContainer } = styles;
 
-  if (editionBreakpoints.small === breakpoint) {
+  if (orientation === "landscape") {
     return (
-      <Fragment>
-        {lead}
-        <VerticalLayout tiles={[support1, support2, support3, support4]} />
-      </Fragment>
+      <View style={styles.landscapeContainer}>
+        <View style={styles.landscapeLeadContainer}>{lead}</View>
+        <ItemColSeparator />
+        <VerticalLayout
+          style={styles.verticalSupportContainer}
+          tiles={[support1, support2, support3, support4]}
+        />
+      </View>
     );
   }
 
   return (
-    <View style={container}>
-      <View style={leadContainer}>{lead}</View>
+    <View style={styles.portraitContainer}>
+      <View style={styles.portraitLeadContainer}>{lead}</View>
       <ItemColSeparator />
-      <VerticalLayout
-        style={supportContainer}
-        tiles={[support1, support2, support3, support4]}
+      <HorizontalLayout
+        containerStyle={styles.horizontalSupportContainer}
+        tiles={[
+          { style: styles.supportItem, tile: support1 },
+          { style: styles.supportItem, tile: support2 },
+          { style: styles.supportItem, tile: support3 },
+          { style: styles.supportItem, tile: support4 },
+        ]}
       />
     </View>
   );
