281115c94d4663193ecc86581da5e792c06aeaf6
Aron Shamash <aronshamash@gmail.com>
2020-09-14 12:02:37 +0100

    fix: label alignment on indepth

--- packages/article-in-depth/src/styles/index.js
+++ packages/article-in-depth/src/styles/index.js
@@ -1,33 +1,102 @@
 import { StyleSheet } from "react-native";
-import { spacing } from "@times-components-native/styleguide";
-import sharedStyles from "./shared";
+import styleguide, {
+  tabletWidth,
+  tabletWidthMax,
+} from "@times-components-native/styleguide";
 
-const nativeStyles = {
-  ...sharedStyles,
+const { colours, fontFactory, spacing } = styleguide();
+const styles = StyleSheet.create({
+  articleHeadline: {
+    ...fontFactory({
+      font: "headline",
+      fontSize: "headline",
+    }),
+    color: colours.functional.brandColour,
+    marginBottom: spacing(2),
+    textAlign: "center",
+  },
   container: {
-    ...sharedStyles.container,
+    alignItems: "center",
+    paddingBottom: spacing(9),
+    paddingHorizontal: spacing(4),
     paddingTop: spacing(9),
   },
-  standFirst: {
-    ...sharedStyles.standFirst,
-    lineHeight: 25,
-    marginBottom: 0,
-  },
-  metaContainer: {
-    ...sharedStyles.metaContainer,
-    marginTop: spacing(4),
+  containerTablet: {
+    alignSelf: "center",
+    maxWidth: tabletWidthMax,
   },
   datePublication: {
-    ...sharedStyles.datePublication,
-    marginTop: 0,
+    ...fontFactory({
+      font: "supporting",
+      fontSize: "cardMeta",
+    }),
+    color: colours.functional.secondary,
+  },
+  datePulicationTablet: {
+    marginTop: spacing(0),
+  },
+  headerText: {
+    alignItems: "center",
+  },
+  headerTextTablet: {
+    width: tabletWidth,
+    alignItems: "flex-start",
+    borderWidth: 4,
   },
   label: {
-    marginBottom: 0,
+    marginBottom: spacing(2),
   },
-};
-
-const styles = StyleSheet.create({
-  ...nativeStyles,
+  leadAsset: {
+    marginBottom: spacing(0),
+  },
+  leadAssetTablet: {
+    alignSelf: "center",
+    maxWidth: tabletWidthMax,
+    width: "100%",
+  },
+  meta: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  metaContainer: {
+    alignItems: "center",
+    marginHorizontal: spacing(2),
+    paddingBottom: spacing(2),
+    marginTop: spacing(4),
+    borderBottomColor: colours.functional.keyline,
+    borderBottomWidth: 1,
+    borderTopColor: colours.functional.keyline,
+    borderTopWidth: 1,
+    marginBottom: spacing(4),
+  },
+  metaContainerTablet: {
+    alignSelf: "center",
+    width: tabletWidth,
+  },
+  metaContainerTabletFlow: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+  },
+  separator: {
+    borderRightColor: colours.functional.keyline,
+    borderRightWidth: 1,
+    height: spacing(3),
+    marginRight: spacing(2),
+    paddingLeft: spacing(2),
+  },
+  standFirst: {
+    ...fontFactory({
+      font: "headlineRegular",
+      fontSize: "smallestHeadline",
+    }),
+    color: colours.functional.primary,
+    paddingHorizontal: spacing(2),
+    textAlign: "center",
+    lineHeight: 25,
+  },
 });
 
 export default styles;
