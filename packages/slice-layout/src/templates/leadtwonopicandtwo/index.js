f7db950bf439b6016289ffd617b5e1179b8516cd
Chris Jordan <chris.jordan@news.co.uk>
2020-10-05 09:29:54 +0100

    fix: fixes lead2nopicand2 on wide portrait (#176)

--- packages/slice-layout/src/templates/leadtwonopicandtwo/index.js
+++ packages/slice-layout/src/templates/leadtwonopicandtwo/index.js
@@ -8,17 +8,19 @@ import VerticalLayout from "../verticallayout";
 
 const LeadTwoNoPicAndTwoSlice = ({
   breakpoint,
+  orientation,
   lead1,
   lead2,
   support1,
   support2,
 }) => {
-  const styles = styleFactory(breakpoint);
+  const styles = styleFactory(orientation);
 
-  if (
-    breakpoint === editionBreakpoints.wide ||
-    breakpoint === editionBreakpoints.huge
-  ) {
+  if (breakpoint === editionBreakpoints.small) {
+    return <VerticalLayout tiles={[lead1, lead2, support1, support2]} />;
+  }
+
+  if (orientation === "landscape") {
     return (
       <View style={styles.container}>
         <VerticalLayout style={styles.column} tiles={[lead1, lead2]} />
@@ -30,17 +32,14 @@ const LeadTwoNoPicAndTwoSlice = ({
     );
   }
 
-  if (breakpoint === editionBreakpoints.medium) {
-    return (
-      <View style={styles.container}>
-        <VerticalLayout style={styles.column} tiles={[lead1, lead2]} />
-        <ItemColSeparator style={styles.colSeparatorStyle} />
-        <VerticalLayout style={styles.column} tiles={[support2, support1]} />
-      </View>
-    );
-  }
-
-  return <VerticalLayout tiles={[lead1, lead2, support1, support2]} />;
+  // portrait tablet
+  return (
+    <View style={styles.container}>
+      <VerticalLayout style={styles.column} tiles={[lead1, lead2]} />
+      <ItemColSeparator style={styles.colSeparatorStyle} />
+      <VerticalLayout style={styles.column} tiles={[support2, support1]} />
+    </View>
+  );
 };
 
 LeadTwoNoPicAndTwoSlice.propTypes = {
