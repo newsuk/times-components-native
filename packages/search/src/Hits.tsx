b6770d720e534651027a896ed20c0051d8d9825e
Chris Hutchinson <chris_hutchinson@me.com>
2020-11-11 22:36:06 +0000

    Fix missed rebase

--- packages/search/src/Hits.tsx
+++ packages/search/src/Hits.tsx
@@ -8,16 +8,11 @@ import ArticleList from "@times-components-native/article-list";
 export const Hits: any = connectSearchBox(
   connectInfiniteHits(
     ({ hits, refineNext, hasMore, onArticlePress, currentRefinement }: any) => {
-<<<<<<< HEAD
       const [hasFinished, setHasFinished] = React.useState(false);
 
       const handleFetchMore = () => {
         if (!hasMore) {
           setHasFinished(true);
-=======
-      const handleFetchMore = () => {
-        if (!hasMore) {
->>>>>>> feat: initial pass at a new Algolia powered search page
           return Promise.resolve();
         }
 
