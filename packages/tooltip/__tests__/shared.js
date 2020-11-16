8919705923ddba353759ed2197c792c62481bbde
Aron Shamash <aronshamash@gmail.com>
2020-11-16 16:07:07 +0000

    feat: add tests

--- packages/tooltip/__tests__/shared.js
+++ packages/tooltip/__tests__/shared.js
@@ -1,6 +1,5 @@
 import Tooltip from "../tooltip";
 import TestRenderer from "react-test-renderer";
-import { ResponsiveContext } from "@times-components-native/responsive";
 import { delay } from "@times-components-native/test-utils";
 import { shallow } from "enzyme";
 import { TouchableOpacity, Animated } from "react-native";
@@ -8,71 +7,39 @@ import React from "react";
 import { Text } from "react-native";
 import "./serializers-with-all-styles";
 
-export const withTabletContext = (WrappedComponent, isTablet = true) => (
-  <ResponsiveContext.Provider
-    value={{
-      isTablet: isTablet,
-    }}
-  >
-    {WrappedComponent}
-  </ResponsiveContext.Provider>
-);
-
 export default () => {
   describe("Tooltip", () => {
-    it("renders correctly when type is in tooltips array", () => {
+    it("renders correctly", () => {
       const onTooltipPresentedMock = jest.fn();
 
       const output = TestRenderer.create(
-        withTabletContext(
-          <Tooltip
-            content={<Text>foo</Text>}
-            onTooltipPresented={onTooltipPresentedMock}
-            type="testtype"
-            tooltips={["testtype"]}
-          >
-            bar
-          </Tooltip>,
-        ),
+        <Tooltip
+          content={<Text>foo</Text>}
+          onTooltipPresented={onTooltipPresentedMock}
+          type="testtype"
+          tooltips={["testtype"]}
+        >
+          bar
+        </Tooltip>,
       );
       expect(output).toMatchSnapshot();
     });
 
-    it("renders correctly when type is not in tooltips array", () => {
+    it("renders correctly width supplied width", () => {
       const onTooltipPresentedMock = jest.fn();
 
       const output = TestRenderer.create(
-        withTabletContext(
-          <Tooltip
-            content={<Text>foo</Text>}
-            onTooltipPresented={onTooltipPresentedMock}
-            type="testtype"
-            tooltips={[""]}
-          >
-            bar
-          </Tooltip>,
-        ),
+        <Tooltip
+          content={<Text>foo</Text>}
+          onTooltipPresented={onTooltipPresentedMock}
+          type="testtype"
+          tooltips={["testtype"]}
+          width="100"
+        >
+          bar
+        </Tooltip>,
       );
-      expect(output.toJSON()).toEqual("bar");
-    });
-
-    it("does not render tooltip if not in tablet", () => {
-      const onTooltipPresentedMock = jest.fn();
-
-      const output = TestRenderer.create(
-        withTabletContext(
-          <Tooltip
-            content={<Text>foo</Text>}
-            onTooltipPresented={onTooltipPresentedMock}
-            type="testtype"
-            tooltips={["testtype"]}
-          >
-            bar
-          </Tooltip>,
-          false,
-        ),
-      );
-      expect(output.toJSON()).toEqual("bar");
+      expect(output).toMatchSnapshot();
     });
 
     it("fades out when clicking the close button", async () => {
@@ -112,6 +79,27 @@ export default () => {
       );
       testInstance.children().at(0).props().onViewportEnter();
       expect(onTooltipPresentedMock).toBeCalled();
+    });
+
+    it("onClose is called correctly", async () => {
+      const onTooltipPresentedMock = jest.fn();
+      const onCloseMock = jest.fn();
+
+      const testInstance = shallow(
+        <Tooltip
+          content={<Text>foo</Text>}
+          onClose={onCloseMock}
+          onTooltipPresented={onTooltipPresentedMock}
+          type="testtype"
+          tooltips={["testtype"]}
+        >
+          bar
+        </Tooltip>,
+      );
+
+      const closeButton = testInstance.find(TouchableOpacity);
+      closeButton.simulate("press");
+      expect(onCloseMock).toBeCalled();
     });
   });
 };
