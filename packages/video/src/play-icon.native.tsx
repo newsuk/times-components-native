7f927cd47f49641e5f098fd1496599a0eac3ce5f
Kevin Amiranoff <kamiranoff@gmail.com>
2021-01-07 12:11:11 +0000

    update tests and small refactoring

--- packages/video/src/play-icon.native.tsx
+++ packages/video/src/play-icon.native.tsx
@@ -12,20 +12,21 @@ export type PlayIconProps = {
   containerWidth?: number;
 };
 
+const getSize = (containerWidth: number) => {
+  switch (true) {
+    case containerWidth > 270:
+      return PLAY_ICON_SIZE.MEDIUM;
+    case containerWidth > 563:
+      return PLAY_ICON_SIZE.LARGE;
+    case containerWidth > 755:
+      return PLAY_ICON_SIZE.XLARGE;
+    default:
+      return PLAY_ICON_SIZE.SMALL;
+  }
+};
+
 const PlayIcon: FC<PlayIconProps> = ({ containerWidth = 0 }) => {
-  const getSize = () => {
-    switch (true) {
-      case containerWidth > 270:
-        return PLAY_ICON_SIZE.MEDIUM;
-      case containerWidth > 563:
-        return PLAY_ICON_SIZE.LARGE;
-      case containerWidth > 755:
-        return PLAY_ICON_SIZE.XLARGE;
-      default:
-        return PLAY_ICON_SIZE.SMALL;
-    }
-  };
-  const size = getSize();
+  const size = getSize(containerWidth);
   return (
     <Svg width={size} height={size} viewBox="0 0 64 64" fill="none">
       <Rect opacity="0.6" width="64" height="64" fill="#01000D" />
