aa17d6abb572887e1559e4633a8bdaa58e3605b9
Chris Jordan <chris.jordan@news.co.uk>
2021-01-18 11:56:36 +0000

    refactor: makes addSliceConfig easier to extend

--- packages/section/src/utils/addSliceConfig.ts
+++ packages/section/src/utils/addSliceConfig.ts
@@ -1,22 +1,42 @@
 import { TileConfig } from "@times-components-native/types";
 
+interface Slice {
+  support: {
+    config: TileConfig;
+  };
+}
+
+interface TransformSlice {
+  index: number;
+  name: string;
+  sectionTitle: string;
+  transform: (slice: Slice) => Slice;
+}
+
+const transformLeadOneAndOneNews = {
+  sectionTitle: "News",
+  index: 0,
+  name: "LeadOneAndOneSlice",
+  transform: (slice: Slice) => ({
+    ...slice,
+    support: { ...slice.support, config: { showImage: true } },
+  }),
+};
+
+const sliceTransformations: TransformSlice[] = [transformLeadOneAndOneNews];
+
 export const addSliceConfig = (isTablet: boolean, sectionTitle: string) => (
   slices: any[],
 ) => {
   if (!isTablet) return slices;
 
   return slices.map((slice, index) => {
-    if (
-      sectionTitle.match(/News/i) &&
-      slice.name === "LeadOneAndOneSlice" &&
-      index == 0
-    ) {
-      const tileConfig: TileConfig = { showImage: true };
-      return {
-        ...slice,
-        support: { ...slice.support, config: tileConfig },
-      };
-    }
-    return slice;
+    const transformation = sliceTransformations.find(
+      (st) =>
+        st.index === index &&
+        st.name == slice.name &&
+        st.sectionTitle.toUpperCase() === sectionTitle.toUpperCase(),
+    );
+    return transformation?.transform(slice) ?? slice;
   });
 };
