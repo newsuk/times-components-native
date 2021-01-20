f1d54e62290a11e9d09eb31da8f7a1bac154203b
justadamhopkins <adamhopkins87@gmail.com>
2021-01-20 16:23:21 +0000

    refactor: transform slice

--- packages/section/src/utils/transformSlice.ts
+++ packages/section/src/utils/transformSlice.ts
@@ -1,5 +1,28 @@
 import { TileConfig } from "@times-components-native/types";
-import { imageAspectRatios } from "./sectionConfigs";
+import merge from "lodash.merge";
+
+// configure slice
+
+const base = {
+  support1: {
+    summary: { length: 800 },
+    showImage: true,
+    wide: { image: { ratio: "3:2", orientation: "landscape" } },
+    huge: { image: { ratio: "3:2", orientation: "landscape" } },
+    medium: {},
+  },
+  support2: {
+    image: { ratio: "2:3", orientation: "portrait" },
+    showImage: true,
+    wide: {},
+    huge: { image: { ratio: "4:5", orientation: "landscape" } },
+    medium: {},
+  },
+};
+
+const baseConfigs = {
+  LeadTwoNoPicAndTwoSlice: base,
+};
 
 interface Slice {
   support: {
@@ -16,34 +39,37 @@ interface TransformSlice {
   transform: (slice: Slice) => Slice;
 }
 
-const leadOneAndOneNewsTransform = {
-  sectionTitle: "News",
-  name: "LeadOneAndOneSlice",
-  transform: (slice: Slice) => ({
-    ...slice,
-    support: { ...slice.support, config: { showImage: true } },
-  }),
-};
+// const leadOneAndOneNewsTransform = {
+//   sectionTitle: "News",
+//   name: "LeadOneAndOneSlice",
+//   transform: (slice: Slice) => ({
+//     ...slice,
+//     support: { ...slice.support, config: { showImage: true } },
+//   }),
+// };
 
 const leadTwoNoPicAndTwoVariant2SportTransform = {
-  sectionTitle: "sport",
+  sectionTitle: "world",
   name: "LeadTwoNoPicAndTwoSlice",
-  transform: (slice: Slice) => ({
-    ...slice,
+  overrides: {
     support1: {
-      ...slice.support1,
-      config: {
-        summary: { length: 800 },
-        wide: { image: { ratio: "3:2", orientation: "landscape" } },
-        huge: { image: { ratio: "3:2", orientation: "landscape" } },
-        medium: {},
-      },
+      summary: { length: "summaryMerge" },
+      showImage: false,
+      wide: { image: { ratio: "dog", orientation: "landscape" } },
+      huge: { image: { ratio: "cat", orientation: "landscape" } },
+      medium: {},
     },
-  }),
+    support2: {
+      image: { ratio: "2:3", orientation: "portrait" },
+      showImage: "falsy",
+      wide: {},
+      huge: { image: { ratio: "elephant", orientation: "landscape" } },
+      medium: {},
+    },
+  },
 };
 
 const sliceTransformations: TransformSlice[] = [
-  leadOneAndOneNewsTransform,
   leadTwoNoPicAndTwoVariant2SportTransform,
 ];
 
@@ -57,5 +83,34 @@ export const transformSlice = (isTablet: boolean, sectionTitle: string) => (
       st.name == slice.name &&
       st.sectionTitle.toUpperCase() === sectionTitle.toUpperCase(),
   );
-  return transformation?.transform(slice) ?? slice;
+
+  // merge
+  // if no base
+  // console.log(
+  //   merge(
+  //     baseConfigs[slice.name],
+  //     transformation.overrides[slice.name],
+  //     "trying to merge",
+  //   ),
+  // );
+  if (!transformation || !baseConfigs[transformation.name]) return slice;
+
+  const test = {
+    ...slice,
+    config: merge(baseConfigs[slice.name], transformation.overrides),
+  };
+
+  // console.log(test);
+
+  return transformation?.overrides
+    ? {
+        ...slice,
+        config: merge(baseConfigs[slice.name], transformation.overrides),
+      }
+    : {
+        ...slice,
+        config: {
+          ...baseConfigs[slice.name],
+        },
+      };
 };
