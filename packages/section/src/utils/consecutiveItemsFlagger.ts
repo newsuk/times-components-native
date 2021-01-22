386e0536c632b6779d619354096f9c22a833f754
Chris Jordan <chris.jordan@news.co.uk>
2021-01-22 15:09:02 +0000

    feat: flips secondary-four after lead slices

--- packages/section/src/utils/consecutiveItemsFlagger.ts
+++ packages/section/src/utils/consecutiveItemsFlagger.ts
@@ -2,15 +2,30 @@ import memoizeOne from "memoize-one";
 
 const withIsConsecutive = (slice: any) => ({ ...slice, isConsecutive: true });
 
+// in order to
+const consecutivePairs = [
+  ["TopSecondaryFourSlice", "SecondaryFourSlice"],
+  ["LeadOneAndOneSlice", "SecondaryFourSlice"],
+];
+
+const isConsecutivePair = (
+  currentName: string,
+  previousName: string,
+): boolean =>
+  consecutivePairs.some(([a, b]) => previousName == a && currentName == b);
+
 export const consecutiveItemsFlagger = memoizeOne((slices: any[]) =>
-  slices.reduce(
-    (acc, curr, i) =>
-      acc.length > 0 &&
-      curr.name &&
-      acc[i - 1].name &&
-      curr.name === acc[i - 1].name
-        ? [...acc, withIsConsecutive(curr)]
-        : [...acc, curr],
-    [],
-  ),
+  slices.reduce((acc, curr, i) => {
+    const currentName = curr.name;
+    const previousName = acc[i - 1]?.name;
+    const previousIsConsecutive = acc[i - 1]?.isConsecutive;
+
+    const isConsecutiveSlices =
+      currentName === previousName ||
+      isConsecutivePair(currentName, previousName);
+
+    return !previousIsConsecutive && isConsecutiveSlices
+      ? [...acc, withIsConsecutive(curr)]
+      : [...acc, curr];
+  }, []),
 );
