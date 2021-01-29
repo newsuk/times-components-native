35b25957c26f899b83a594ef752f02e960ff0fe8
Geoff Ford <me@geoffford.co.uk>
2021-01-29 15:44:52 +0000

    chore: Migrate drop cap to new approach (#311)
    * Migrate DropCaps from old absolute approach to new inline approach
    
    * Dropcap fixes for mobile
    
    * Tweaks to work consistently across all platforms and breakpoints
    
    * More tweaks for scaling of dropcaps and removal of old dropcap approaches
    
    * Remainder of drop cap work
    
    * Some android drop cap tweaks
    
    * Small change for ST on iOS

--- packages/article-paragraph/__tests__/shared.js
+++ packages/article-paragraph/__tests__/shared.js
@@ -1,7 +1,3 @@
-import React from "react";
-import { Text } from "react-native";
-import TestRenderer from "react-test-renderer";
-import { scales } from "@times-components-native/styleguide";
 import {
   addSerializers,
   compose,
@@ -10,14 +6,7 @@ import {
   flattenStyleTransform,
   print,
 } from "@times-components-native/jest-serializer";
-import { iterator } from "@times-components-native/test-utils";
 import shared from "./shared.base";
-import "./mock-text-measure-module";
-import DropCap from "../src/drop-cap";
-
-const mockDropCap = "I";
-const mockText =
-  "n 1924 Harold Macmillan became MP for Stockton-on-Tees. Witnessing brutal poverty there between the wars, he said later that he had learnt “lessons which I have never forgotten. ";
 
 export default () => {
   addSerializers(
@@ -30,34 +19,5 @@ export default () => {
     ),
   );
 
-  iterator([
-    ...shared(),
-    {
-      name: "re-measures when scale changes",
-      test: async () => {
-        const testInstance = TestRenderer.create(
-          <DropCap
-            dropCap={mockDropCap}
-            scale={scales.large}
-            text={mockText}
-          />,
-        );
-
-        expect(
-          testInstance.root.findAllByType(Text)[0].props.style[0].fontSize,
-        ).toBe(115);
-        testInstance.update(
-          <DropCap
-            dropCap={mockDropCap}
-            scale={scales.xlarge}
-            text={mockText}
-          />,
-        );
-
-        expect(
-          testInstance.root.findAllByType(Text)[0].props.style[0].fontSize,
-        ).toBe(124);
-      },
-    },
-  ]);
+  shared();
 };
--- packages/article-paragraph/src/styles/shared.js
+++ packages/article-paragraph/src/styles/shared.js
@@ -1,14 +1,10 @@
 import styleguide, {
   scales,
-  fonts,
   tabletWidth,
 } from "@times-components-native/styleguide";
-import { fontSize, margins } from "./drop-cap-sizes";
 
-const sharedStyles = (dropCapFont = "dropCap", scale = scales.medium) => {
+const sharedStyles = (scale = scales.medium) => {
   const { colours, fontFactory, spacing } = styleguide({ scale });
-  const dropCapMargins = margins(dropCapFont, scale);
-  const dropCapFontSize = fontSize(dropCapFont, scale);
 
   return {
     articleMainContentRow: {
@@ -36,21 +32,6 @@ const sharedStyles = (dropCapFont = "dropCap", scale = scales.medium) => {
     narrow: {
       alignSelf: "flex-start",
       width: "100%",
-    },
-    dropCapContainer: {
-      flexDirection: "row",
-      flexWrap: "wrap",
-    },
-    dropCapContainerTablet: {
-      width: tabletWidth,
-    },
-    dropCapTextElement: {
-      color: colours.functional.primary,
-      fontFamily: fonts[dropCapFont],
-      fontSize: dropCapFontSize,
-      marginBottom: dropCapMargins.bottom,
-      marginRight: spacing(1),
-      marginTop: dropCapMargins.top,
     },
   };
 };
