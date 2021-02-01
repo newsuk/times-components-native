3edf9f9d8910e80372f170b8562ad7f1e0f4ce2b
Geoff Ford <me@geoffford.co.uk>
2021-02-01 18:33:48 +0000

    chore: Increase minimum screen width used for styling tablets (#325)
    * Increase minimum screen width used for styling tablets
    
    * Updated snapshots
    
    * Fixes bug in tooltip and associated tests
    
    * Updated common inline-content snapshots

--- packages/tooltip/__tests__/ios/tooltip.ios.test.js
+++ packages/tooltip/__tests__/ios/tooltip.ios.test.js
@@ -1,65 +1,66 @@
-import Tooltip from "../../../tooltip";
-import { ResponsiveContext } from "@times-components-native/responsive";
 import { delay } from "@times-components-native/test-utils";
-import { shallow } from "enzyme";
-import { TouchableOpacity, Animated } from "react-native";
 import React from "react";
-import { Text } from "react-native";
-import "../serializers-with-all-styles";
-import { Platform } from "react-native";
+import { Animated } from "react-native";
+import TestRenderer from "react-test-renderer";
+import { Platform, Text } from "react-native";
 
-export const withTabletContext = (WrappedComponent, isTablet = true) => (
-  <ResponsiveContext.Provider
-    value={{
-      isTablet: isTablet,
-    }}
-  >
-    {WrappedComponent}
-  </ResponsiveContext.Provider>
-);
+import "../serializers-with-all-styles";
+
+import Tooltip from "../../../tooltip";
+import { withTabletContext } from "../shared";
 
 describe("Tooltip", () => {
   Platform.OS = "ios";
   it("fades out when clicking the close button", async () => {
     const onTooltipPresentedMock = jest.fn();
 
-    const output = shallow(
-      <Tooltip
-        content={<Text>foo</Text>}
-        onTooltipPresented={onTooltipPresentedMock}
-        type="testtype"
-        tooltips={["testtype"]}
-      >
-        bar
-      </Tooltip>,
+    const output = TestRenderer.create(
+      withTabletContext(
+        <Tooltip
+          content={<Text>foo</Text>}
+          onTooltipPresented={onTooltipPresentedMock}
+          type="testtype"
+          tooltips={["testtype"]}
+        >
+          bar
+        </Tooltip>,
+      ),
     );
 
-    const closeButton = output.find(TouchableOpacity);
-    closeButton.simulate("press");
+    const closeButton = output.root.findByProps({
+      testID: "closeButton",
+    });
+    closeButton.props.onPress();
     await delay(250);
-    expect(output.find(Animated.View).get(0).props.style.opacity._value).toBe(
-      0,
-    );
+
+    expect(
+      output.root.findByType(Animated.View).children[0].props.style.opacity
+        ._value,
+    ).toBe(0);
   });
 
   it("onClose is called correctly", async () => {
     const onTooltipPresentedMock = jest.fn();
     const onCloseMock = jest.fn();
 
-    const testInstance = shallow(
-      <Tooltip
-        content={<Text>foo</Text>}
-        onClose={onCloseMock}
-        onTooltipPresented={onTooltipPresentedMock}
-        type="testtype"
-        tooltips={["testtype"]}
-      >
-        bar
-      </Tooltip>,
+    const output = TestRenderer.create(
+      withTabletContext(
+        <Tooltip
+          content={<Text>foo</Text>}
+          onClose={onCloseMock}
+          onTooltipPresented={onTooltipPresentedMock}
+          type="testtype"
+          tooltips={["testtype"]}
+        >
+          bar
+        </Tooltip>,
+      ),
     );
 
-    const closeButton = testInstance.find(TouchableOpacity);
-    closeButton.simulate("press");
+    const closeButton = output.root.findByProps({
+      testID: "closeButton",
+    });
+    closeButton.props.onPress();
     expect(onCloseMock).toBeCalled();
   });
 });
