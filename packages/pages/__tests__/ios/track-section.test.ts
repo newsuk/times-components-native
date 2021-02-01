d7c14f59dd9d6cf1176418a06df78228562f8faa
Kevin Amiranoff <kamiranoff@gmail.com>
2021-02-01 18:09:03 +0000

    update tests for track-article and track-section

---
+++ packages/pages/__tests__/ios/track-section.test.ts
@@ -0,0 +1,21 @@
+import { NativeModules } from "react-native";
+import "./../mock-track";
+import trackSection from "@times-components-native/pages/src/section/track-section";
+
+describe("pages - track sections", () => {
+  it("section page view tracking calls only onSectionLoaded", () => {
+    const {
+      SectionEvents: { onSectionLoaded },
+      ReactAnalytics: { track },
+    } = NativeModules;
+
+    trackSection({
+      action: "Viewed",
+      attrs: { sectionName: "dummy-section-name" },
+      object: "Section",
+    });
+
+    expect(onSectionLoaded).toHaveBeenCalled();
+    expect(track).toHaveBeenCalled();
+  });
+});
