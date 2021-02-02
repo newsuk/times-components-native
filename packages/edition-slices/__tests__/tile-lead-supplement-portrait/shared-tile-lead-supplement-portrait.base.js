df5b70442b9a62319a23b4eac2e066f2c041fecd
Aron Shamash <aronshamash@gmail.com>
2021-02-02 11:47:30 +0000

    chore: update snaps

--- packages/edition-slices/__tests__/tile-lead-supplement-portrait/shared-tile-lead-supplement-portrait.base.js
+++ packages/edition-slices/__tests__/tile-lead-supplement-portrait/shared-tile-lead-supplement-portrait.base.js
@@ -1,22 +1,16 @@
 import "../mocks-tiles";
 import { editionBreakpoints } from "@times-components-native/styleguide";
 import { testTile } from "../shared-tile-utils";
-import { TileAZ } from "../../src/tiles";
+import { TileLeadSupplementPortrait } from "../../src/tiles";
 
 export default () => {
   describe("tile lead supplement portrait", () => {
-    describe("portrait", () => {
-      it("medium", () => {
-        testTile(TileAZ, editionBreakpoints.medium, undefined, {
-          orientation: "portrait",
-        });
-      });
+    it("medium", () => {
+      testTile(TileLeadSupplementPortrait, editionBreakpoints.medium);
+    });
 
-      it("wide", () => {
-        testTile(TileAZ, editionBreakpoints.wide, undefined, {
-          orientation: "portrait",
-        });
-      });
+    it("wide", () => {
+      testTile(TileLeadSupplementPortrait, editionBreakpoints.wide);
     });
   });
 };
