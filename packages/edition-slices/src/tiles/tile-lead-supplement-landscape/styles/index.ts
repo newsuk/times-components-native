6b5f59134f4f2d07db65fb3e0ae4dd47ebeda493
Chris Jordan <chris.jordan@news.co.uk>
2021-02-02 11:01:56 +0000

    Fixes types

--- packages/edition-slices/src/tiles/tile-lead-supplement-landscape/styles/index.ts
+++ packages/edition-slices/src/tiles/tile-lead-supplement-landscape/styles/index.ts
@@ -4,7 +4,7 @@ import {
   spacing,
   editionBreakpoints,
 } from "@times-components-native/styleguide";
-import { TextStyle } from "react-native";
+import { StyleSheet } from "react-native";
 
 const fontSizeResolver: Record<string, number> = {
   [editionBreakpoints.medium]: 38,
@@ -12,28 +12,29 @@ const fontSizeResolver: Record<string, number> = {
   [editionBreakpoints.huge]: 40,
 };
 
-export default (breakpoint: string) => ({
-  container: {
-    flex: 1,
-    flexDirection: "row",
-    backgroundColor: colours.functional.border,
-  },
-  headline: {
-    color: colours.functional.brandColour,
-    fontFamily: fonts.headlineRegular,
-    fontWeight: "normal",
-    fontSize: fontSizeResolver[breakpoint],
-    lineHeight: fontSizeResolver[breakpoint],
-  } as TextStyle,
-  teaserText: {
-    fontSize: 15,
-    lineHeight: 22,
-    color: colours.functional.brandColour,
-    fontFamily: fonts.bodyRegular,
-  },
-  summaryContainer: {
-    flex: 1,
-    backgroundColor: colours.functional.whriteGrey,
-    padding: spacing(6),
-  },
-});
+export default (breakpoint: string) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      flexDirection: "row",
+      backgroundColor: colours.functional.border,
+    },
+    headline: {
+      color: colours.functional.brandColour,
+      fontFamily: fonts.headlineRegular,
+      fontWeight: "normal",
+      fontSize: fontSizeResolver[breakpoint],
+      lineHeight: fontSizeResolver[breakpoint],
+    },
+    teaserText: {
+      fontSize: 15,
+      lineHeight: 22,
+      color: colours.functional.brandColour,
+      fontFamily: fonts.bodyRegular,
+    },
+    summaryContainer: {
+      flex: 1,
+      backgroundColor: colours.functional.whriteGrey,
+      padding: spacing(6),
+    },
+  });
