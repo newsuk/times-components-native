d23478f7a8c09b03897ea12ef453499cd2d57b62
Chris Jordan <chris.jordan@news.co.uk>
2021-02-03 14:10:37 +0000

    type fixes

--- packages/edition-slices/src/tiles/tile-lead-supplement-portrait/styles/index.ts
+++ packages/edition-slices/src/tiles/tile-lead-supplement-portrait/styles/index.ts
@@ -4,41 +4,43 @@ import {
   spacing,
   editionBreakpoints,
 } from "@times-components-native/styleguide";
+import { StyleSheet } from "react-native";
 
-const fontSizeResolver = {
+const fontSizeResolver: Record<string, number> = {
   [editionBreakpoints.medium]: 40,
   [editionBreakpoints.wide]: 45,
 };
 
-const horizontalPaddingResolver = {
+const horizontalPaddingResolver: Record<string, number> = {
   [editionBreakpoints.medium]: spacing(10),
   [editionBreakpoints.wide]: spacing(28),
 };
 
-export default (breakpoint, summaryHeight) => ({
-  container: {
-    flex: 1,
-    borderBottomWidth: 1,
-    borderBottomColor: colours.functional.keyline,
-  },
-  headline: {
-    color: colours.functional.brandColour,
-    fontFamily: fonts.headlineRegular,
-    fontWeight: "normal",
-    fontSize: fontSizeResolver[breakpoint],
-    lineHeight: fontSizeResolver[breakpoint],
-    textAlign: "center",
-  },
-  imageContainer: {
-    width: "100%",
-  },
-  summaryContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: colours.functional.border,
-    paddingHorizontal: horizontalPaddingResolver[breakpoint],
-    paddingVertical: spacing(4),
-    height: summaryHeight,
-  },
-});
+export default (breakpoint: string, summaryHeight: number) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      borderBottomWidth: 1,
+      borderBottomColor: colours.functional.keyline,
+    },
+    headline: {
+      color: colours.functional.brandColour,
+      fontFamily: fonts.headlineRegular,
+      fontWeight: "normal",
+      fontSize: fontSizeResolver[breakpoint],
+      lineHeight: fontSizeResolver[breakpoint],
+      textAlign: "center",
+    },
+    imageContainer: {
+      width: "100%",
+    },
+    summaryContainer: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: colours.functional.border,
+      paddingHorizontal: horizontalPaddingResolver[breakpoint],
+      paddingVertical: spacing(4),
+      height: summaryHeight,
+    },
+  });
