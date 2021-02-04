10b25b910e28020ebb5fb48ecd492d305e67c840
Chris Jordan <chris.jordan@news.co.uk>
2021-02-04 13:44:42 +0000

    corrects type

--- packages/edition-slices/src/tiles/shared/tile-link.tsx
+++ packages/edition-slices/src/tiles/shared/tile-link.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from "react";
 import Link from "@times-components-native/link";
-import { ViewStyle } from "react-native";
+import { StyleProp, ViewStyle } from "react-native";
 import { OnArticlePress } from "@times-components-native/types";
 import { Tile } from "@times-components-native/fixture-generator/src/types";
 
 interface Props {
   onPress: OnArticlePress;
-  style?: ViewStyle;
+  style?: StyleProp<ViewStyle>;
   tile: Tile;
 }
 const TileLink: FC<Props> = ({
