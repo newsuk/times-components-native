bc12a5221d2a608d86b92b556c6b341140328c4f
Geoff-Ford <me@geoffford.co.uk>
2021-02-23 13:32:32 +0000

    Commented out Android specific dimensions code and updated RN

--- packages/utils/src/dimensions-util.js
+++ packages/utils/src/dimensions-util.js
@@ -1,15 +1,15 @@
 import {
   Dimensions,
-  NativeEventEmitter,
-  NativeModules,
-  Platform,
+  // NativeEventEmitter,
+  // NativeModules,
+  // Platform,
 } from "react-native";
 
-let eventEmitter;
+// let eventEmitter;
 
-if (Platform.OS === "android") {
-  eventEmitter = new NativeEventEmitter(NativeModules.ReactNativeEvent);
-}
+// if (Platform.OS === "android") {
+//   eventEmitter = new NativeEventEmitter(NativeModules.ReactNativeEvent);
+// }
 
 export const getDimensions = (
   width = Dimensions.get("window").width,
@@ -18,28 +18,28 @@ export const getDimensions = (
 ) => ({ width, height, fontScale });
 
 export const addDimensionsListener = (type, handler) => {
-  if (Platform.OS === "android") {
-    const wrappedHandler = (data) => {
-      return handler({
-        window: {
-          width: data.displayWidth,
-          height: data.displayHeight,
-          fontScale: data.fontScale,
-        },
-      });
-    };
-    eventEmitter.addListener("configurationChanged", wrappedHandler);
-    return wrappedHandler;
-  } else {
-    Dimensions.addEventListener(type, handler);
-    return handler;
-  }
+  // if (Platform.OS === "android") {
+  //   const wrappedHandler = (data) => {
+  //     return handler({
+  //       window: {
+  //         width: data.displayWidth,
+  //         height: data.displayHeight,
+  //         fontScale: data.fontScale,
+  //       },
+  //     });
+  //   };
+  //   eventEmitter.addListener("configurationChanged", wrappedHandler);
+  //   return wrappedHandler;
+  // } else {
+  Dimensions.addEventListener(type, handler);
+  return handler;
+  // }
 };
 
 export const removeDimensionsListener = (type, handler) => {
-  if (Platform.OS === "android") {
-    eventEmitter.removeListener("configurationChanged", handler);
-  } else {
-    Dimensions.removeEventListener(type, handler);
-  }
+  // if (Platform.OS === "android") {
+  //   eventEmitter.removeListener("configurationChanged", handler);
+  // } else {
+  Dimensions.removeEventListener(type, handler);
+  // }
 };
