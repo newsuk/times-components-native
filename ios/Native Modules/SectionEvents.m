2a33dc852d2306835c7f233dc791f2d602ff27ec
Kevin Amiranoff <kamiranoff@gmail.com>
2021-03-02 18:22:09 +0000

    add missing SectionEvents methods in ios

--- ios/Native Modules/SectionEvents.m
+++ ios/Native Modules/SectionEvents.m
@@ -22,7 +22,47 @@ RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(onSectionLoaded:(NSString *) sectionName event:(NSDictionary *) event)
 {
-  NSLog(@"onSectionLoaded  %@ %@", sectionName, event);
+  NSLog(@"onSectionLoaded %@ %@", sectionName, event);
+}
+
+RCT_EXPORT_METHOD(onArticlePress:(NSString *) url isPuff:(NSString *) isPuff)
+{
+  NSLog(@"onArticlePress %@ %@", url, isPuff);
+}
+
+RCT_EXPORT_METHOD(onArticleLoaded:(NSString *) articleId extras: (NSDictionary *) extras)
+{
+  NSLog(@"onArticleLoaded %@ %@", articleId, extras);
+}
+
+RCT_EXPORT_METHOD(onAuthorPress:(NSString *) slug)
+{
+  NSLog(@"onAuthorPress %@", slug);
+}
+
+RCT_EXPORT_METHOD(onLinkPress:(NSString *) url)
+{
+  NSLog(@"onLinkPress %@", url);
+}
+
+RCT_EXPORT_METHOD(onVideoPress:(NSDictionary *) info)
+{
+  NSLog(@"onVideoPress %@", info);
+}
+
+RCT_EXPORT_METHOD(onTopicPress:(NSString *) url)
+{
+  NSLog(@"onTopicPress %@", url);
+}
+
+RCT_EXPORT_METHOD(onCommentsPress:(NSString *) articleId url:(NSString *) url)
+{
+  NSLog(@"onCommentsPress %@ %@",articleId, url);
+}
+
+RCT_EXPORT_METHOD(onCommentGuidelinesPress)
+{
+  NSLog(@"onCommentGuidelinesPress");
 }
 
 @end
