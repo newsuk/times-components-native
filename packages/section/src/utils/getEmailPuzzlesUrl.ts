ea04c8a2e7161911f7964ac5436e246646b08406
Aron Shamash <aronshamash@gmail.com>
2021-03-25 10:21:56 +0000

    chore: (TNLT-6184) Remove unused email puzzle button (#387)
    * chore: remove unused email puzzle button
    
    * chore: revert onLinkPress params
    
    * chore: update snaps
    
    * chore: remove unused exports

--- packages/section/src/utils/getEmailPuzzlesUrl.ts
+++
@@ -1,9 +0,0 @@
-import format from "date-fns/format";
-
-export const getEmailPuzzlesUrl = (publishedTime: any) =>
-  publishedTime
-    ? `https://times.formstack.com/forms/puzzles_${format(
-        publishedTime,
-        "DD_MM_YYYY",
-      )}`
-    : ``;
