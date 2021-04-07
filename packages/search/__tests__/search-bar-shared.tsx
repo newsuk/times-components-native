a1205e6b89a206cb9a9b8bb3c81a4ffeb35735a1
Mario Norato <norato.mario@gmail.com>
2021-04-07 16:30:27 +0100

    fix: fixed failing checks

--- packages/search/__tests__/search-bar-shared.tsx
+++ packages/search/__tests__/search-bar-shared.tsx
@@ -2,7 +2,7 @@ import TestRenderer from "react-test-renderer";
 import React from "react";
 import EmptySearchMessage from "../src/empty-search-message";
 import CancelButton from "../src/search-bar/cancel-button";
-import { SearchBar } from "../src/search-bar/search-bar";
+import { SearchBarComponent } from "../src/search-bar/search-bar";
 
 export default () => {
   it("EmptySearchMessage", () => {
@@ -12,13 +12,20 @@ export default () => {
 
   describe("Search Bar", () => {
     it("should render correctly", () => {
-      const testInstance = TestRenderer.create(<SearchBar />);
+      const testInstance = TestRenderer.create(
+        <SearchBarComponent
+          isConnected={true}
+          refine={() => null}
+          isSearchStalled={false}
+          currentRefinement={"test"}
+        />,
+      );
       expect(testInstance).toMatchSnapshot();
     });
 
     it("CancelButton", () => {
       const testInstance = TestRenderer.create(
-        <CancelButton onPress={() => null} />,
+        <CancelButton onPress={() => null} isConnected={true} />,
       );
       expect(testInstance).toMatchSnapshot();
     });
