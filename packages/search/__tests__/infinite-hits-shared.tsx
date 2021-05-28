cc5fe01e6838a2d3909b130417311dda5f62ee37
Peter <masonptr@gmail.com>
2021-05-28 12:58:03 +0100

    update search test for ios

--- packages/search/__tests__/infinite-hits-shared.tsx
+++ packages/search/__tests__/infinite-hits-shared.tsx
@@ -1,6 +1,15 @@
 import TestRenderer from "react-test-renderer";
 import React from "react";
+import { mockNativeModules } from "@times-components-native/mocks";
 import SearchResults from "../src/search-results";
+
+// mockNativeModules();
+
+jest.mock("react-native", () => {
+  const rn = jest.requireActual("react-native");
+  rn.NativeModules.ReactAnalytics = { track: jest.fn() };
+  return rn;
+});
 
 export default () => {
   describe("SearchResults", () => {
