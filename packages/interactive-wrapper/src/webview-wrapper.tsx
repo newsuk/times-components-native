6c62d909e84c4d3e3005c9382177f1efae507600
Peter <masonptr@gmail.com>
2021-06-16 14:08:49 +0100

    wip

---
+++ packages/interactive-wrapper/src/webview-wrapper.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+// import { WebView } from "react-native-webview";
+
+function WebviewWrapper() {
+  return (
+    <View>
+      <Text>Web view wrapper</Text>
+    </View>
+  );
+}
+
+export default WebviewWrapper;
