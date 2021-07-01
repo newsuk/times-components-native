b86b363e6144a18d8d8288631e7bd9b6a6772a21
Peter <masonptr@gmail.com>
2021-07-01 15:11:00 +0100

    remove console

--- packages/article-skeleton/src/article-body/article-end-tracking.js
+++ packages/article-skeleton/src/article-body/article-end-tracking.js
@@ -14,7 +14,6 @@ const ArticleEndTracking = ({ onViewed }) => {
     <ViewportAwareView
       testID="viewportAwareView"
       onViewportEnter={() => {
-        console.log("onviewed");
         onViewed();
       }}
     />
