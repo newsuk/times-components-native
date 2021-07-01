800423e98724e66ac9b052629f78876bd56ca1ee
Stuart McCamley <s.mccamley@gmail.com>
2021-07-01 16:10:45 +0100

    done

--- packages/article-summary/index.js
+++ packages/article-summary/index.js
@@ -1,11 +1,21 @@
-export {
-  default,
+import ArticleSummary, {
   ArticleSummaryContent,
   ArticleSummaryHeadline,
   ArticleSummaryLabel,
   ArticleSummaryStrapline,
   summarise,
 } from "./src/article-summary";
+import renderer from "./src/article-summary-renderer";
+import { renderAst } from "./src/renderAst";
 
-export { renderAst } from "./src/renderAst";
-export { renderer } from "./src/article-summary-renderer";
+export {
+  renderAst,
+  renderer,
+  ArticleSummaryContent,
+  ArticleSummaryHeadline,
+  ArticleSummaryLabel,
+  ArticleSummaryStrapline,
+  summarise,
+};
+
+export default ArticleSummary;
